8d8143f9b47df68aef58f31f49728de5d58582c7
Chris Ince <chris@ince.live>
2023-03-31 20:19:58 -0500

    Try relative url for webhooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import AudioRecorder from "./AudioRecorder.js";
 
-function App() {
 
-  const [socket] = React.useState(new WebSocket('ws://localhost:8001'))
+const loc = window.location;
+var new_uri;
+if (window.location.protocol === "https:") {
+    new_uri = "wss:";
+} else {
+    new_uri = "ws:";
+}
+new_uri += "//" + window.location.host;
+new_uri += window.location.pathname + "api";
+
+
+
+function App() {
+  const [socket] = React.useState(new WebSocket(new_uri))
 
   return (
     <div>
